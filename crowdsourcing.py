616e8261801fad02eddb971212d60170c4683c7a
Marco Willi <will5448@umn.edu>
2019-03-13 20:10:12 -0500

    use custom json dumper for numpy datatypes

--- crowdsourcing.py
+++ crowdsourcing.py
@@ -32,6 +32,22 @@ import os
 import numpy as np
 import collections
 
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+            np.int16, np.int32, np.int64, np.uint8,
+            np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32,
+            np.float64)):
+            return float(obj)
+        elif isinstance(obj,(np.ndarray,)): #### This is the fix
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
 class CrowdImage(object):
     """ An image to be annotated.
     """
@@ -693,8 +709,11 @@ class CrowdDataset(object):
                         'image_id': i,
                         'label': self.images[i].y.encode()
                     })
+
+        dumped = json.dumps(data, cls=NumpyEncoder)
+
         with open(fname, 'w') as f:
-            json.dump(data, f)
+            json.dump(dumped, f)
 
     def parse(self, data):
         for k in data:
